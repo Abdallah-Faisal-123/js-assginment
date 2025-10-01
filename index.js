e8979025d20820db789e1711b3b907f75c4c588e
Abdallah Fiasal123 <abdallah.fisal@gmail.com>
2025-10-01 21:49:38 +0300

    js-Question-2

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-/*================= question 1 ===================*/
-var num = window.prompt("enter your input");
-console.log(num)
+/*================= question 2 ===================*/
+var num = +window.prompt("enter number that can divide by 3 and 4");
+
+if(num % 4 === 0 && num % 3 === 0 )
+{
+   console.log("Output: Yes");
+}
+else
+{
+    console.log("Output: No");
+}
